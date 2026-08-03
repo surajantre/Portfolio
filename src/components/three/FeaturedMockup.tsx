692c41743ba5b29a15d641806a010bf0562ef974
SURAJ ANTRE <surajantre1234@gmail.com>
2026-08-03 12:05:50 +0530

    Fix PresentationControls snap prop

--- src/components/three/FeaturedMockup.tsx
+++ src/components/three/FeaturedMockup.tsx
@@ -25,7 +25,7 @@ function GlassScreen({ title, category }: { title: string; category: string }) {
         polar={[-0.4, 0.2]}
         azimuth={[-1, 0.75]}
         config={{ mass: 2, tension: 400 }}
-        snap={{ mass: 4, tension: 400 }}
+        snap
       >
         <Float rotationIntensity={0.4}>
           {/* Outer Screen Frame */}
